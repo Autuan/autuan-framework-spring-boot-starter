96c7bcaa9df1f1d05b2476c8e02427cbbb5771de
Autuan <yu@autuan.top>
2023-12-19 16:06:15 +0800

    ShortChain 获取原始数据

--- src/main/java/top/autuan/shortchain/ShortChainComponent.java
+++ src/main/java/top/autuan/shortchain/ShortChainComponent.java
@@ -10,59 +10,19 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 public class ShortChainComponent {
     public static final String base62Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    private RedissonClient redissonClient;
-
     private final boolean RECURSION;
 
-    public ShortChainComponent(RedissonClient redissonClient,boolean recursion) {
+    private final int DAY_TO_LIVE ;
+    private RedissonClient redissonClient;
+
+    public ShortChainComponent(RedissonClient redissonClient, boolean recursion) {
         this.redissonClient = redissonClient;
-        this.RECURSION=recursion;
-    }
-
-    // 生成一个不重复的短链/短码
-    public String generateShortCode(String bloomFilterKey,String originData,boolean isRecursion) {
-        // 一个系统可能有多个短链，所以需要指定 bloomFilterKey 如: 用户邀请码  文章公告短链 分享短链
-        bloomFilterKey = Optional.ofNullable(bloomFilterKey).orElse("bloom:filter:key");
-
-        List<String> list = new ArrayList<>();
-        // 一次性生成20个 尽量避免递归
-        for (int i = 0; i < 20; i++) {
-            String randomStr = RandomUtil.randomString(12);
-            originData += randomStr;
-
-            // 通过 Guava 进行 MurmurHash 计算
-            long hash = Hashing.murmur3_32().hashString(originData, StandardCharsets.UTF_8).padToLong();
-            // 转为 62 进制
-            String hash62 = convertToBase62(hash);
-            list.add(hash62);
-        }
-
-        // 是否递归
-            RBloomFilter<Object> filter = redissonClient.getBloomFilter(bloomFilterKey);
-            // 避免过滤器未初始化
-            if(!filter.isExists() ){
-                filter.tryInit(2048L, 0.03);
-            }
-
-            for (String val : list) {
-                if (filter.contains(val)) {
-                    continue;
-                } else {
-                    // 不存在，存入 redis
-                    filter.add(val);
-                    return val;
-                }
-            }
-
-            if(isRecursion) {
-                return generateShortCode(bloomFilterKey, originData, isRecursion);
-            }
-
-        throw new BusinessException("generator error repeat 20 times");
+        this.RECURSION = recursion;
+        this.DAY_TO_LIVE = 90;
     }
 
     private static String convertToBase62(long decimal) {
@@ -73,5 +33,69 @@ public class ShortChainComponent {
         } while (decimal > 0);
 
         return result;
+    }
+
+    // 生成一个不重复的短链/短码
+    public String generateShortCode( String originData) {
+        return generateShortCode(null, originData, RECURSION);
+    }
+
+    public String generateShortCode(String bloomFilterKey, String originData) {
+        return generateShortCode(bloomFilterKey, originData, RECURSION);
+    }
+
+    public String generateShortCode(String bloomFilterKey, String originData, boolean isRecursion) {
+        // 一个系统可能有多个短链，所以需要指定 bloomFilterKey 如: 用户邀请码  文章公告短链 分享短链
+        bloomFilterKey = Optional.ofNullable(bloomFilterKey).orElse("bloom:filter:key");
+        String reflectionKey = bloomFilterKey+ ":reflection:";
+
+        List<String> list = new ArrayList<>();
+        // 一次性生成20个 尽量避免递归
+        for (int i = 0; i < 20; i++) {
+            String randomStr = RandomUtil.randomString(12);
+            String ramdomDataStr = originData +  randomStr;
+
+            // 通过 Guava 进行 MurmurHash 计算
+            long hash = Hashing.murmur3_32().hashString(ramdomDataStr, StandardCharsets.UTF_8).padToLong();
+            // 转为 62 进制
+            String hash62 = convertToBase62(hash);
+            list.add(hash62);
+        }
+
+        RBloomFilter<Object> filter = redissonClient.getBloomFilter(bloomFilterKey);
+        // 避免过滤器未初始化
+        if (!filter.isExists()) {
+            filter.tryInit(2048L, 0.03);
+        }
+
+        for (String val : list) {
+            if (filter.contains(val)) {
+                continue;
+            } else {
+                // 不存在，存入 redis
+                filter.add(val);
+                redissonClient.getBucket(reflectionKey + val).set(originData,DAY_TO_LIVE, TimeUnit.DAYS);
+                return val;
+            }
+        }
+
+        if (isRecursion) {
+            return generateShortCode(bloomFilterKey, originData, isRecursion);
+        }
+
+        throw new BusinessException("generator error repeat 20 times");
+    }
+
+    public String accessData( String shortChain){
+        return accessData(null, shortChain);
+    }
+
+    public String accessData(String bloomFilterKey , String shortChain){
+        bloomFilterKey = Optional.ofNullable(bloomFilterKey).orElse("bloom:filter:key");
+        String reflectionKey = bloomFilterKey+ ":reflection:";
+
+        Object o = redissonClient.getBucket(reflectionKey + shortChain)
+                .get();
+        return String.valueOf(o);
     }
 }
